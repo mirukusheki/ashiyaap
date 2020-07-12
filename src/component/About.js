c5e3ec719b2c1aa5da4e0640b8583c2fd05adf01
Ryuujo <41565025+ryuujo@users.noreply.github.com>
2020-07-12 21:32:28 +0700

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -6,15 +6,14 @@ import React from 'react';
 const About = () => {
   return (
     <div className="social-media">
-      Created by <b>MirukuSheki</b> <br/>
-      <b>Github:</b> <a href="https://github.com/mirukusheki" rel="noopener">mirukusheki</a><br/>
-      <b>Paypal: </b><a href="https://paypal.me/RJKuker?locale.x=id_ID" rel="noopener">paypal.me/RJKuker</a><br/>
+      Created by <b>Ryuujo</b> <br/>
+      <b>Github:</b> <a href="https://github.com/ryuujo" rel="noopener">Ryuujo</a><br/>
       <small>(You can Donate to me for more development or just enjoying my web page)</small>
       <br/>
-      <b>Repositories:</b> <a href="https://github.com/mirukusheki/ashiyaap"
-                              rel="noopener">https://github.com/mirukusheki/ashiyaap</a>
+      <b>Repositories:</b> <a href="https://github.com/ryuujo/ashiyaap"
+                              rel="noopener">https://github.com/Ryuryuujoujo/ashiyaap</a>
       <br/><br/>
-      Found any bug? <a href="https://github.com/mirukusheki/ashiyaap/issues" rel="noopener">Let me know</a>
+      Found any bug? <a href="https://github.com/ryuujo/ashiyaap/issues" rel="noopener">Let me know</a>
     </div>
   );
 };
